e8a8c7f8550adad34ea22b92781758bfbdb94b2a
Nam Tran <tranphuongnam.TPN@gmail.com>
2025-08-27 23:05:32 +0700

    Init project with client + server

---
+++ client/README.md
@@ -0,0 +1,1 @@
+This is base for NEXTJS project.
---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      {" "}
+      This is NAMTRAN BASE PAGE
+      <div>
+        <Button type="primary"> Hello</Button>
+      </div>
+    </div>
+  );
+}
